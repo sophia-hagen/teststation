756e39d118d1840ac1d18d3d7a690dce72577f82
sophia-hagen <hagen.sophia04@gmail.com>
2024-01-08 15:06:05 +0100

    neue Version SensorOutput

--- firmware/SensorOutput/SensorOut.py
+++ firmware/SensorOutput/SensorOut.py
@@ -1,7 +1,31 @@
-import STS1_Sensors as Sensors
-from time import sleep
+from STS1_Sensors import Sensors
+import smbus2
 
-for i in range(1,7):
-    sen = Sensors.SensorsData()
-    print(sen)
-    sleep(1)
+# Initialize I2C bus
+bus = smbus2.SMBus(1)
+
+# Create an instance of the Sensors class
+sensors = Sensors(bus)
+
+# Set up the sensors
+sensors.setup()
+
+# Get the sensor data
+sensors.getData()
+
+# Retrieve and print the sensor data
+data = sensors.output
+print("Temperature (TMP112):", data.tempTMP)
+print("Temperature (BME688):", data.tempBME)
+print("Humidity:", data.hum)
+print("Pressure:", data.press)
+print("Gas Resistance:", data.gasRes)
+print("Acceleration X:", data.accX)
+print("Acceleration Y:", data.accY)
+print("Acceleration Z:", data.accZ)
+print("Gyroscope X:", data.gX)
+print("Gyroscope Y:", data.gY)
+print("Gyroscope Z:", data.gZ)
+print("UVA:", data.uva)
+print("UVA Index:", data.uvaI)
+# Add more print statements for other sensor data as needed
